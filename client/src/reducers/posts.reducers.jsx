cf2d77e1de257d2a81df47da5ad2d7283972748d
stormzy22 <aawweb23@gmail.com>
2021-09-29 09:11:50 +0100

    ready to host

--- client/src/reducers/posts.reducers.jsx
+++ client/src/reducers/posts.reducers.jsx
@@ -3,13 +3,13 @@ import { CREATE, DELETE, FETCH_ALL, UPDATE } from "../constants/actionTypes";
 const reducer = (posts = [], action) => {
   switch (action.type) {
     case DELETE:
-      return posts.filter((post) => post._id !== action.payload);
+      return posts?.filter((post) => post?._id !== action?.payload);
     case UPDATE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+      return posts?.map((post) => (post?._id === action?.payload._id ? action?.payload : post));
     case FETCH_ALL:
-      return action.payload;
+      return action?.payload;
     case CREATE:
-      return [...posts, action.payload];
+      return [...posts, action?.payload];
     default:
       return posts;
   }
