493ead20f52c0c6e363dc3efb502ddae69c0d889
stormzy22 <aawweb23@gmail.com>
2021-10-15 16:31:29 +0100

    refactored

--- client/src/actions/posts.actions.jsx
+++ client/src/actions/posts.actions.jsx
@@ -6,7 +6,6 @@ export const getPost = (id) => async (dispatch) => {
     dispatch({ type: START_LOADING });
     const { data } = await api.fetchPost(id);
     dispatch({ type: FETCH_POST, payload: data });
-    console.log(data);
     dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
@@ -17,7 +16,6 @@ export const getPosts = (page) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING });
     const { data } = await api.fetchPosts(page);
-    console.log(data);
     dispatch({ type: FETCH_ALL, payload: data });
     dispatch({ type: END_LOADING });
   } catch (error) {
